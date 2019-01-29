c5013537d2905ac6880dde75b0ccaa936427622a
GigaMatt <msmontoya1996@gmail.com>
2019-01-28 20:18:33 -0700

    Added intro notes from lab 1

---
+++ intro.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+main(){
+
+		/*
+		 * 3 different lab assignements
+		 * Guidelines given; the rest is up to us
+		 * GRADING: Graded by proficiency in skills
+		 * 	- Skills involved with projectwork
+		 * 		-Converting C code to assembly
+		 * 			**Ability to encode instructions
+		 * 			**Semantics (chosing the right operation [x+y] -> add)
+		 * 		-0.0 (F): Didn't show any skill
+		 * 		-0.5 (F): You still don't got it
+		 * 		-1.0 (F): You know/got something
+		 * 		-1.5 (C): You know a little more than something
+		 * 		-2.0 (A): You got it
+		 * 		-3.0 (A+): Creativly solved the problem (i.e. compiler ompitmization)
+		 *
+		 * Normal to have lower scores at beginning of course on quizes
+		 * Graded off of the end of the semester grades => to prove you know it at the end
+		 * 
+		 *
+		 * DANIELS OFFICE HOURS: 
+		 * 	-M/T: 10:30-12:00
+		 * 	-R:   11:00-12:00
+		 * /
+
+	/*
+	 * Project 1 Assigned February 4th
+	 * Why learn C?
+	 * 	-Different Language
+	 * 	-Performace
+	 * 		*Faster than Java
+	 * 	-Better Coverage
+	 * 	-Improved Job Prospects (resume + skills)
+	 * 	-Broaden Horizons
+	 * 	-Languages Interfaces w/
+	 * 		*Smartcars
+	 * 		*Missiles
+	 * 		*Anything with systems/real-time programming
+	 */ 
+
+		/*
+	 	* Bash
+	 	* 	-Linux shell
+	 	*	-Good for SSH/remote access
+	 	*	-More powerful; allows access to everything
+		* Commands
+		* 	-man	
+		* 		-man provides manuel on that specific command
+		* 	LEARN:
+		* 		-mv
+		* 		-cp
+		* 		-mkdir
+		* 		-cat
+		* 		-emacs
+	 	*/ 
+}
