78ca4c3478aeb449b4962a3961590c1c6bfbe0e7
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-02-01 20:28:39 -0700

    Added notes to test file on header+.c files && compilation, execution

--- introduction/test.c
+++ introduction/test.c
@@ -1,9 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>	//header file
 
-/*
+/**
+ * .h = HEADER FILES
+ * 		CONTAIN METHOD SIGNITURES (.h)
+ * 
+ *		Similar to "import" in Java
+ * 		Similar to "includes" in Java
+ * 			i.e. .compareTo(Object e)
+ * 
+ * .c = C FILE
+ * 		THEY CONTAIN DECLARATIONS
+ * 			i.e. "This is my logic and how I declare it."
+ * 
+ * #include = (LITERALLY INCLUDES FILE)
+ * 		INCLUDE HEADER FILES INTO OUR .C FILE
+ * 		Includes all the packages
+ *
  * TO EXECUTE CODE:
- * 1) gcc program_name.c -o my_program_name
- * 2) ./my_program_name
+ * 		1) gcc program_name.c -o my_program_name
+ * 		2) ./my_program_name
 */
 
 int main(){
