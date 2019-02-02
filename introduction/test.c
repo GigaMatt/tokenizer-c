6dacd542effdb3149f9b58c2a4757795db46d6f2
Matthew Montoya <msmontoya2@miners.utep.edu>
2019-02-01 22:16:47 -0700

    Added sample arithmetic and varible declaration function

--- introduction/test.c
+++ introduction/test.c
@@ -21,11 +21,38 @@
  * 		2) ./my_program_name
 */
 
+
 int main(){
-	void print();
+	void print_hello();
+	void arithmetic_variables();
 	return 0;
 }
 
 void print_hello(){
-	printf("Welcome to my first C Program");)
+	//		\n necessary b.c. C has no default "new line" value (i.e. print() vs println() in Java)
+	printf("Welcome to my first C Program\n");
+}
+
+/**
+ * SIMPLE VARIABLE DECLARATIONS AND WHILE-LOOPING
+ */ 
+void arithmetic_variables(){
+	//Multiple declarations similar like in Java
+	int faren, celc;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	faren=lower;
+	
+	//Calculate celc values given faren values
+	printf("Degrees F\tDegrees C")
+
+	while(faren <= upper){
+		celc = 5*(faren-32)/9;
+		printf("%d\t%d\n", faren, celc);
+		faren+= step;	
+	}
 }
